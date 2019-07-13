1185ecd0a0ead1c41b3396b6d6bad2652e4db21e
Anshul Tripathi <atripa.asu@gmail.com>
2019-07-13 18:21:42 -0400

    Update 88.MergeSortedArray.py

--- 88.MergeSortedArray.py
+++ 88.MergeSortedArray.py
@@ -7,15 +7,18 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        if not nums2:
-            return nums1
-        i = 0
-        j = 0
-        length = len(nums1)
-        while i < m:
-            if nums1[i] > nums2[0]:
-                nums1[i],nums2[0] = nums2[0],nums1[i]
-                nums2 = sorted(nums2)
-            i+=1
-        for l,k in enumerate(range(i,length)):
-            nums1[k] = nums2[l]
+        # if not nums2:
+        #     return nums1
+        # i = 0
+        # j = 0
+        # length = len(nums1)
+        # while i < m:
+        #     if nums1[i] > nums2[0]:
+        #         nums1[i],nums2[0] = nums2[0],nums1[i]
+        #         nums2 = sorted(nums2)
+        #     i+=1
+        # for l,k in enumerate(range(i,length)):
+        #     nums1[k] = nums2[l]
+        
+        nums1[m:] = nums2
+        nums1.sort()
